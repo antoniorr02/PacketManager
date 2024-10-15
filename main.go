44aff5b1bdd6ecccb4287e1e695ec060ce464b8c
antoniorr <antoniorr@correo.ugr.es>
2024-10-15 10:55:13 +0200

    Definición de clientes, issue #7

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// Definición de la entidad Cliente
+type Cliente struct {
+	Nombre   string // Nombre del cliente
+	DNI      string // DNI como identificador único del cliente
+	Telefono string // Número de teléfono del cliente
+}
+
 func main() {
 	name := "mundo"
 	fmt.Println("Hola", name)
