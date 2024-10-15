b53db8ea9a5643387d6f058f9a2b1236e38e9129
antoniorr <antoniorr@correo.ugr.es>
2024-10-15 10:58:38 +0200

    Identificación de sedes con su respectivo stock #8

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ type Cliente struct {
 	Telefono string // Número de teléfono del cliente
 }
 
+// Palet representa un tipo de palet con una cantidad específica
+type Palet struct {
+	Tipo     string
+	Cantidad int
+}
+
+// Sede representa una sede de UPalet con un stock de palets
+type Sede struct {
+	ID     int               // ID único de la sede
+	Nombre string            // Nombre de la sede
+	Stock  map[string]*Palet // Mapa de stock de palets, con el tipo como clave
+}
+
 func main() {
 	name := "mundo"
 	fmt.Println("Hola", name)
