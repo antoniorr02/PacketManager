e81a326eb1f2c9097591cb2016b402c4a5e18e75
antoniorr <antoniorr@correo.ugr.es>
2024-10-25 13:19:19 +0200

    Modelado de usuarios en base a #7, #8, #9, #9, #10 y #11

--- internal/usuario.go
+++ internal/usuario.go
@@ -1,9 +1,17 @@
 package internal
 
-// Definición de la entidad Usuario con diferentes roles
+type TipoUsuario string
+
+const (
+	Cliente       TipoUsuario = "cliente"
+	Encargado     TipoUsuario = "encargado"
+	Administrador TipoUsuario = "administrador"
+	Transportista TipoUsuario = "transportista"
+)
+
 type Usuario struct {
-	Nombre   string // Nombre del usuario
-	DNI      string // DNI como identificador único del usuario
-	Telefono string // Número de teléfono del usuario
-	Rol      string // Rol ya sea usuario, encargado, administrador o transportista.
+	Nombre   string      // Nombre del usuario
+	DNI      string      // DNI como identificador único del usuario
+	Telefono string      // Número de teléfono del usuario
+	Rol      TipoUsuario // Rol ya sea cliente, encargado, administrador o transportista.
 }
