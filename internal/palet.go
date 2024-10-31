4baace4d94183f706738f44198cf672b342f3f0a
antoniorr <antoniorr@correo.ugr.es>
2024-10-31 20:31:10 +0100

    Se definen las sedes y los palets que contendrán, issue #8

---
+++ internal/palet.go
@@ -0,0 +1,28 @@
+package internal
+
+type TipoPalet int
+
+const (
+	Palet100x100Barra TipoPalet = iota
+	PaletPerimetral114x114
+	PaletPerimetralFuerte120x100
+	PaletPerimetralFino120x100
+	PaletCP9
+	PaletCP7
+	PaletCP6
+	PaletCP4
+	PaletCP3
+	PaletCP1
+	Palet80x60Dusseldorf
+	Palet90x90Perimetral
+	Palet120x100AbiertoSemiFuerte
+	PaletAmericano120x100AbiertoFuerteConVuelo
+	PaletAmericano120x100AbiertoExtrafuerte
+	PaletAmericano120x100AbiertoFuerte
+	PaletAmericanoPatinLargo120
+	PaletAmericanoPatinCorto100
+	Palet120x80Fino
+	Palet120x80Fuerte
+	EuroPaletHomologadoEPAL
+	EuroPaletHomologado1aExtra
+)
