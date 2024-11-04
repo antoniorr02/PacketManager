fdc5facc9df04b8045af4fd8e20b38ac035576f4
antoniorr <antoniorr@correo.ugr.es>
2024-11-04 17:33:05 +0100

    Corrección del modelo de Pedido, Closes #8

--- internal/pedido.go
+++ internal/pedido.go
@@ -1,19 +1,10 @@
 package internal
 
-type EstadoPedido string
-
-const (
-	Pendiente  EstadoPedido = "Pendiente"
-	Confirmado              = "Confirmado"
-	Entregado               = "Entregado"
-	Cancelado               = "Cancelado"
-)
-
 type Pedido struct {
 	cliente   Usuario
 	encargado Usuario
 	Pedido    map[TipoPalet]int
-	Estado    EstadoPedido
+	Estado    string
 	Id        int
 	Factura   float64
 }
