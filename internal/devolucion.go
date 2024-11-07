ec4e1da8edf7b7d428adeae1904098a37397a886
antoniorr <antoniorr@correo.ugr.es>
2024-11-07 13:19:17 +0100

    Lenguaje ubicuo en relación a #10

--- internal/devolucion.go
+++ internal/devolucion.go
@@ -3,7 +3,7 @@ package internal
 type Devolucion struct {
 	cliente       Usuario
 	transportista Usuario
-	Devolucion    map[TipoPalet]map[string]int // String marca el estado
+	Productos     map[TipoPalet]map[string]int // String marca el estado
 	Compensacion  float64
 	Confirmacion  *Confirmacion
 	PedidoID      int
